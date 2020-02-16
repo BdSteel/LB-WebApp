4cceb68b454ead2edefa1fe2a5007c50c36944cc
Matt Vermeer <matt@legerity.com.au>
2020-02-16 17:07:02 +1100

    CLB added, ASG added, comments

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 # TF-UPGRADE-TODO: Block type was not recognized, so this block and its contents were not automatically upgraded.
+#Choose Cloud Provider and region to deploy to
 provider "aws" {
   region = "us-east-2"
 }
 
+#Create an AWS Security Group allowing HTTP traffic to/from ALL
 resource "aws_security_group" "instance" {
   name = "webserver-instance"
 
@@ -14,6 +16,7 @@ resource "aws_security_group" "instance" {
   }
 }
 
+#Launch config of a web server from a blank Ubuntu image
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
@@ -29,6 +32,7 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
+#ASG for 2-10 nodes
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.id
   availability_zones = ["us-east-2a", "us-east-2b"]
@@ -38,11 +42,12 @@ resource "aws_autoscaling_group" "example" {
 
   tag {
     key                 = "Name"
-    value               = "terraform-WebServer-example"
+    value               = "WebServer Node"
     propagate_at_launch = true
   }
 }
 
+#Create a web server from a base vanilla ubuntu image
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
@@ -59,3 +64,38 @@ user_data = <<-EOF
   }
 }
 
+#Create a CLB
+resource "aws_elb" "LoadBalancer" {
+  name = "loadBalancer"
+  security_groups = [aws_security_group.elb.id]
+  availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
+
+#Add LB Listeners to listen on 8080, route to instance on 8080
+listener {
+  lb_port = 8080
+  lb_protocol = "http"
+  instance_port = 8080
+  instance_protocol = "http"
+  }
+
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-elb"
+
+  #Allow all outbound
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow inbound HTTP from anywhere
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
