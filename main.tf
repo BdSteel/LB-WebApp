f8aaead6bfb7ab5db6c02adeba303091ca2a5518
Matt Vermeer <matt@legerity.com.au>
2020-02-16 16:17:19 +1100

    ASG (Auto Scaling Groups) added for two AZ's - us-east-2a and us-east-2b

--- main.tf
+++ main.tf
@@ -14,6 +14,35 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones = ["us-east-2a", "us-east-2b"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-WebServer-example"
+    propagate_at_launch = true
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
